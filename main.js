10a75ef2e5614c777080a6af8b01619486ec7f0a
Prasad Pathare <prasadsachinist10@gmail.com>
2025-01-31 09:33:43 +0000

    add prompt/yargs check

--- main.js
+++ main.js
@@ -1,58 +1,10 @@
-import { intro, outro, text, cancel, isCancel } from '@clack/prompts';
-import yargs from './lib/yargs.js'
-import { log } from '@clack/prompts';
+import { intro, outro, log } from "@clack/prompts";
+import prompt from "./lib/prompt.js";
 
-intro("DusterCLI app starting")
+intro("DusterCLI app starting");
 
-const input = await text({
-    message: 'What is the input file?',
-    initialValue: 'input.csv',
-    placeholder: "Enter csv file",
-    validate: (value) => {
-        if (!value.toLowerCase().endsWith('.csv')) {
-            return "Must be a csv file."
-        }
-    }
-});
+const { input, output, errors } = await prompt();
 
-if (isCancel(input)) {
-    cancel('Operation cancelled.');
-    process.exit(0);
-}
+log.success(`${input} ${output} ${errors}`);
 
-const output = await text({
-    message: 'What will be the output file?',
-    initialValue: 'output.csv',
-    placeholder: "Enter csv file",
-    validate: (value) => {
-        if (!value.toLowerCase().endsWith('.csv')) {
-            return "Must be a csv file."
-        }
-    }
-});
-
-if (isCancel(output)) {
-    cancel('Operation cancelled.');
-    process.exit(0);
-}
-
-
-const errors = await text({
-    message: 'What will be the errors file?',
-    initialValue: 'errors.csv',
-    placeholder: "Enter csv file",
-    validate: (value) => {
-        if (!value.toLowerCase().endsWith('.csv')) {
-            return "Must be a csv file."
-        }
-    }
-});
-
-if (isCancel(errors)) {
-    cancel('Operation cancelled.');
-    process.exit(0);
-}
-
-log.success(`${input} ${output} ${errors}`)
-
-outro("Thanks for using our app!")
+outro("Thanks for using our app!");
